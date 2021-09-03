a3a4cebd8ae722e1b333f0f411ed0452356e05ab
Bogdan Tyshkovets <btishkovets@gmail.com>
2021-09-03 20:06:35 +0300

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+
+        open("https://www.google.com/");
+    }
+}
